c5fd82f4a622600eed4299510814a6068b97862c
minamahmood83 <minamahmood83@gmail.com>
2021-04-19 16:49:17 -0700

    create faq.js file Read one

--- server/src/api/faq.js
+++ server/src/api/faq.js
@@ -1,3 +1,9 @@
 const express = require("express");
 
 const router = express.Router();
+// Read All
+router.get("/", (req, res, next) => {});
+
+// Read One
+router.get("/:id", (req, res, next) => {});
+module.exports = router;
