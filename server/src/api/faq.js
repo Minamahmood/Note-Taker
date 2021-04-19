c340e9ffaac0419d4937b514c21d788013408387
minamahmood83 <minamahmood83@gmail.com>
2021-04-19 16:52:59 -0700

    create faq.js file create one

--- server/src/api/faq.js
+++ server/src/api/faq.js
@@ -6,4 +6,10 @@ router.get("/", (req, res, next) => {});
 
 // Read One
 router.get("/:id", (req, res, next) => {});
+
+// Create One
+router.get("/", (req, res, next) => {});
+
+// Update One
+router.get("/:id", (req, res, next) => {});
 module.exports = router;
