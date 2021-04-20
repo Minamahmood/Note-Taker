879f755e9b4f76c010e4ee395525d4bc3724dd75
minamahmood83 <minamahmood83@gmail.com>
2021-04-20 00:11:52 -0700

    add try ,catch to ceate one

--- server/src/api/faqs.js
+++ server/src/api/faqs.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const monk = router("monk");
-
+const joi = require("@hapi/joi");
 const db = monk(process.env.MONGO_URI);
 const faqs = db.get("faqs");
+
+const schema = joi.object({
+    question: joi.string().trim().required(),
+    answer: joi.string().trim().required(),
+    video_url: joi.string().uri(),
+});
+
 const router = express.Router();
+
 // READ All
 router.get("/", async(req, res, next) => {
     try {
@@ -24,10 +32,13 @@ router.get("/:id", (req, res, next) => {
 });
 
 // Create One
-router.post("/", (req, res, next) => {
-    res.json({
-        message: "Hello Create One",
-    });
+router.post("/", async(req, res, next) => {
+    try {
+        const value = await schema.validateAsync(req.body);
+        req.json(value);
+    } catch (error) {
+        next(error);
+    }
 });
 // Update One
 router.put("/:id", (req, res, next) => {
